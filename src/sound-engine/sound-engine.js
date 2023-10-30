ce01a8dcd708a296ba2333b86eeb21c38763eb88
Dev BW <devbw@proton.me>
2023-10-30 11:22:02 +0200

    fix sound bug

--- src/sound-engine/sound-engine.js
+++ src/sound-engine/sound-engine.js
@@ -6,14 +6,17 @@ import soundJson from  '../../res/sound/soundsprite.json'
 
 let sound = null;
 let isInited = false
-
+let isAllowed = false
 const soundIds = {};
 
-window.addEventListener('blur', ()=> isInited && Howler.mute(true))
-window.addEventListener('focus', ()=> isInited && Howler.mute(false))
+window.addEventListener('blur', ()=> (isInited && isAllowed) && Howler.mute(true))
+window.addEventListener('focus', ()=> (isInited && isAllowed) && Howler.mute(false))
 subscribe(ON_SET_SOUNDS_ON_OFF, ({detail: allow}) => onSetSoundsOnOff(allow));
 
-const onSetSoundsOnOff = allow => Howler.mute(!allow);
+const onSetSoundsOnOff = allow => {
+    isAllowed = allow;
+    Howler.mute(!allow)
+}
 
 const initSounds = () => {
     sound = new Howl(soundJson)
